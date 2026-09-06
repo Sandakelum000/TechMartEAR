690dd198e7b48d98e5f84386307a4cd959b09604
Hiran sandakelum <hiransandakelum2003@gmail.com>
2026-09-06 18:30:40 +0530

    Remove hardcoded merchant ID and secret
    Removed hardcoded merchant credentials for security.

--- techmart-core/src/main/java/lk/techmart/core/util/PayHereUtil.java
+++ techmart-core/src/main/java/lk/techmart/core/util/PayHereUtil.java
@@ -8,8 +8,8 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Locale;
 
 public class PayHereUtil {
-    private static final String MERCHANT_ID = "1223972";
-    private static final String MERCHANT_SECRET = "MTg3MTA2ODg1NzcyNTkwNzgyOTEzMDg1Nzk4NzQyNTMyOTg2Njc3";
+    private static final String MERCHANT_ID = "";
+    private static final String MERCHANT_SECRET = "";
     public static final String APP_CURRENCY = "LKR";
     public static final String APP_COUNTRY = "Sri Lanka";
     public static final int PAYMENT_SUCCESS = 2;
